55cd6fea43b81eef429ee48326f5eabfa54ed2ab
Shabab Koohi <sh.k.na.1368@gmail.com>
2022-06-17 03:24:43 +0430

    Update Controller.java

--- session14/src/main/java/com/shabab/hi/Controller.java
+++ session14/src/main/java/com/shabab/hi/Controller.java
@@ -1,11 +1,27 @@
 package com.shabab.hi;
 
+
+
+import io.swagger.annotations.Api;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(tags = "Tag1")
+@Tag(name = "Controller", description = "My Custom Description")
 public class Controller {
 
+    @Operation(summary = "Say Hi")
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success|OK"),
+            @ApiResponse(responseCode = "201", description = "create"),
+            @ApiResponse(responseCode = "409", description = "not found"),})
 
     @GetMapping("/hi")
     public String hi(){
